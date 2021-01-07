d940a7d97a0ae6b7875c8b3df0f83523e4a0b978
UberKaeL <uberkael@gmail.com>
2021-01-07 19:18:14 +0100

    Config: Descripciones

--- Config.py
+++ Config.py
@@ -7,12 +7,9 @@ import tensorflow as tf
 from enum import Enum
 
 
-def debug_off():
-	"Elimina los mensajes de warning de CUDA con tensorflow"
-	tf.get_logger().setLevel('ERROR')
-
-
+# Clases libreria para la configuracion
 class Modo(Enum):
+	"Enum con los distintos modos de funcionamiento de la torreta"
 	deteccion = 0
 	search_destroy = 1
 
@@ -32,27 +29,29 @@ class UI():
 
 
 class VidProp():
-	"Clase para la configuracion de video de salida"
+	"Clase con parametros para la configuracion de video de salida"
 	source = "Entrada/vtest.avi"
 	# source = "Entrada/vtest.avi"
 	# fourcc = cv.VideoWriter_fourcc(*"VP80")
 	# fourcc = cv.VideoWriter_fourcc(*"VP90")
 	# fourcc = cv.VideoWriter_fourcc(*"LAGS")
 	fourcc = cv.VideoWriter_fourcc(*"FFV1")
-	fps = 10
+	fps = 6
 	# resolu = (768, 576)
 	resolu = (1280, 720)
 	mobilenet_10 = (2240, 224)
 	show_fps = False
-	guardar = True
+	guardar =
 
 
 class Tracker():
-	"Clase para los parametros de confianza Tracker"
+	"Clase para los parametros de confianza del Tracker"
 	# Area minima para eliminar ruido de contornos
 	area_min = 100
 	# Tolerancia para comparar areas del objetivo actual y los propuestos
 	tolerancia = 0.25
+	# Persistencia de eliminados en frames
+	persistencia = 10
 
 
 class Motor():
@@ -64,7 +63,7 @@ class Motor():
 
 
 class DNN():
-	"Parametros para las redes neuronales"
+	"Parametros para las distintas redes neuronales"
 	# img_size = 224
 	img_size = 416
 	img_margen = 25
@@ -77,20 +76,29 @@ class DNN():
 	archivo_labels = 'Modelos/coco.names'
 
 	class YoLo():
+		"Parametos para una red de detección completa YoLov3"
 		archivo_modelo = 'Modelos/yolov3.cfg'
 		archivo_pesos = 'Modelos/yolov3.weights'
 
 	class YoLo_tiny():
+		"Parametos para una red de detección completa YoLo-Tiny"
 		archivo_modelo = 'Modelos/yolov3-tiny.cfg'
 		archivo_pesos = 'Modelos/yolov3-tiny.weights'
 
 	class MobileNet():
+		"Parametos para una red de detección completa MobileNet V1"
 		size = (300, 300)
 		archivo_modelo = 'Modelos/mobilenet.caffemodel'
 		archivo_prototxt = 'Modelos/mobilenet.prototxt'
 
 
+# Funciones para configurar el programa
+def debug_off():
+	"Elimina los mensajes de warning de CUDA con tensorflow"
+	tf.get_logger().setLevel('ERROR')
+
+
 def Fullscreen(win_name):
-	"Configura el programa para fullscreen"
+	"Configura el programa una salida fullscreen"
 	cv.namedWindow(win_name, cv.WINDOW_NORMAL)
 	cv.setWindowProperty(win_name, cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
