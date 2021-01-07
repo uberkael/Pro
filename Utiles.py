8f847726e735d47b66866298a65336fadcaf3e01
UberKaeL <uberkael@gmail.com>
2021-01-07 19:27:27 +0100

    Utiles: Mejoras

--- Utiles.py
+++ Utiles.py
@@ -8,26 +8,49 @@ import tensorflow as tf
 import Config
 
 
-def guardar(out, img):
-	"Guarda un video"
+def guardar(out, image):
+	"""Simplifica la forma de guardar un video en el disco duro
+	Extrae las opciones de video desde Config
+	Parametros: <out> VideoWriter de OpenCV y una <image> Mat para guardar"""
 	# Si no usa la GPU lo trasnforma en UMat para usarla
-	# if(isinstance(img, (np.ndarray))):
-	if(not isinstance(img, cv.UMat)):
-		img = cv.UMat(img)
+	# if(isinstance(image, (np.ndarray))):
+	if(not isinstance(image, cv.UMat)):
+		image = cv.UMat(image)
 	# Extrae el size
-	h, w, z = img.get().shape
+	h, w, z = image.get().shape
 	wc, hc = Config.VidProp.resolu
 	# Si la resolucion del frame no es la requerida la cambia
 	if wc != w or hc != h:
-		img = cv.resize(img, (wc, hc))
+		image = cv.resize(image, (wc, hc))
 	# Escribe en el archivo
-	out.write(img)
+	out.write(image)
 
 
 def dimensiones_video(cap):
-	"Devuelve las dimensioones x e y de un video"
+	"""Devuelve las dimensiones width y height de un video
+	Parametros: <cap> VideoCapture de OpenCV"""
 	return [int(cap.get(cv.CAP_PROP_FRAME_WIDTH)),
 			int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))]
+
+
+def dibuja_FPS(image, fps):
+	"""Dibuja los FPS en la imagen para comparar rendimientos
+	Parametros: <fps> un objeto imutils.video.FPS
+	"""
+	fps.update()
+	fps.stop()
+	# print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+	text = "FPS: {:.2f}".format(fps.fps())
+	# text = "Hola que haces"
+	cv.putText(image, text, (0, 15),
+			cv.FONT_HERSHEY_SIMPLEX, 0.5, Config.UI.rojo, 2)
+
+
+def ROI_to_img(frame, roi):
+	"""Devuelve una imagen que contenida en  la Region de Interes
+	Parametros: <roi> un rectangulo de la forma x, y, w, h"""
+	x, y, w, h = roi
+	return frame[y:y+h, x:x+w]
 
 
 def punto_centro(dim):
@@ -37,7 +60,10 @@ def punto_centro(dim):
 
 
 def centros_rectangulos(rectangulos):
-	"Devuelve los centros de los objetivos"
+	"""Devuelve una lista con los centros de los objetivos
+	Utiliza centro_rectangulo como funcion auxiliar
+	parametros: <rectangulos> una lista de rectangulos 2D en formato x, y, w, h
+	"""
 	centros = []
 	# El primer punto es el centro donde apunta la torreta
 	# centros.append(np.array([int(dims[0]//2), int(dims[1]//2)]))
@@ -48,7 +74,7 @@ def centros_rectangulos(rectangulos):
 
 
 def centro_rectangulo(r):
-	"Devuelve el centro de un rectangulo definido con altura y ancho"
+	"""Devuelve el centro de un rectangulo definido con altura y ancho"""
 	x, y, w, h = r
 	cX = (x+x+w)//2
 	cY = (y+y+h)//2
@@ -73,12 +99,12 @@ def distancia(a, b):
 
 
 def cerca(p_actual, punto, distancia_max=Config.Motor.d_disp):
-	"Devuelve un booleano si esta lo suficientemente cerca"
+	"Devuelve un booleano si esta lo suficientemente cerca segun un umbral"
 	return True if(distancia(p_actual, punto) < distancia_max) else False
 
 
 def rectangulo_contenedor(contorno, margen=0):
-	"Devuelve el rectangulo que contiene el contorno"
+	"Devuelve el rectangulo que contiene al contorno"
 	# Calculamos el rectangulo que contiene el elemento
 	peri = cv.arcLength(contorno, True)
 	# Dibuja un rectangulo con su centro
@@ -92,12 +118,6 @@ def rectangulo_contenedor(contorno, margen=0):
 	return np.array([x, y, w, h])
 
 
-def ROI_to_img(frame, ROI):
-	"Devuelve una imagen que contiene solo la Region de Interes"
-	x, y, w, h = ROI
-	return frame[y:y+h, x:x+w]
-
-
 def genera_rectangulos(contornos):
 	"Devuelve todos los rectangulos que contienen a los contornos en una lista"
 	rectangulos = []
@@ -108,73 +128,79 @@ def genera_rectangulos(contornos):
 
 
 def gen_p_aleatorios(num_puntos, dims):
-	"Genera n puntos aleatorios entre unas dimensiones"
+	"Genera n puntos aleatorios entre unas dimensiones x e y"
 	# Para cuadrado
 	# return np.random.randint(0, min(dims[0], dims[1]), (num_puntos, 2))
 	# Para rectangulo
 	return np.random.randint(0, (dims[1], dims[0]), (num_puntos, 2))
 
 
-def dibuja_puntos(imagen, lista_p, destruidos=False):
-	"Dibuja los puntos en la imagen"
+def dibuja_puntos(image, lista_p, destruidos=False):
+	"""Dibuja  una lista de puntos en la imagen
+	Parametros: <destruidos> dibuja puntos destruidos en lugar de objetivos"""
 	for i, punto in enumerate(lista_p):
 		if destruidos:
-			cv.circle(imagen, (punto[0], punto[1]), 5, Config.UI.lima, 3)
-			cv.putText(imagen, str(i),
+			cv.circle(image, (punto[0], punto[1]), 5, Config.UI.lima, 3)
+			cv.putText(image, str(i),
 				(punto[0]+5, punto[1]-5), 0, 1, Config.UI.lima, 2)
 		else:
-			cv.circle(imagen, (punto[0], punto[1]), 5, Config.UI.cyan2, 3)
-			cv.putText(imagen, str(i),
+			cv.circle(image, (punto[0], punto[1]), 5, Config.UI.cyan2, 3)
+			cv.putText(image, str(i),
 				(punto[0]+5, punto[1]-5), 0, 1, Config.UI.rojo_claro, 1, 16)
-	return imagen
+	return image
 
 
 def dibuja_mira(imagen, p_actual):
-	"Dibuja una mira en la imagen"
+	"Dibuja el actual punto de mira en la imagen"
 	cv.drawMarker(imagen, (p_actual[0], p_actual[1]), Config.UI.rojo,
 					cv.MARKER_CROSS, 30, 2)
 	return imagen
 
 
-def dibuja_path(img, lista_p):
-	"Dibuja el path a seguir por la torreta entre distintos objetivos"
+def dibuja_path(image, lista_p):
+	"""Dibuja el path a seguir por la torreta entre distintos objetivos
+	Parametros: <lista_p> una lista de puntos objetivos en orden"""
 	anterior = np.array([])
 	for punto in lista_p:
 		if anterior.size == punto.size:
-			cv.arrowedLine(img, (anterior[0], anterior[1]),
+			cv.arrowedLine(image, (anterior[0], anterior[1]),
 							(punto[0], punto[1]), Config.UI.rojo, 2,
 							tipLength=0.05)
 		anterior = punto
 
 
-def dibuja_contornos(frame, contornos):
-	"""Dibuja
+def dibuja_contornos(image, contornos):
+	"""En una lista de contornos dibuja:
 	El contorno,
 	El centro del contorno,
 	Un rectangulo alrededor,
-	El centro del rectangulo
+	El centro del rectangulo que rodea el contorno
+	Utiliza dibuja_rectangulo() como funcion auxiliar
 	"""
 	for c in contornos:
 		# Dibuja el contorno
-		cv.drawContours(frame, [c], 0, Config.UI.rojo_claro, 1)
+		cv.drawContours(image, [c], 0, Config.UI.rojo_claro, 1)
 		# Dibujas el centro del contorno
 		M = cv.moments(c)
 		cX = int(M["m10"] / M["m00"])
 		cY = int(M["m01"] / M["m00"])
-		cv.circle(frame, (cX, cY), 1, (255, 255, 0), 0)
+		cv.circle(image, (cX, cY), 1, (255, 255, 0), 0)
 		rectangulo = rectangulo_contenedor(c)
-		dibuja_rectangulo(frame, rectangulo)
+		dibuja_rectangulo(image, rectangulo)
 
 
-def dibuja_rectangulo(frame, rectangulo, color=Config.UI.rojo_oscuro):
-	"Dibuja un rectangulo contenedor"
+def dibuja_rectangulo(image, rectangulo, color=Config.UI.rojo_oscuro, fill=1):
+	"Dibuja un rectangulo y su punto central en una imagen"
 	x, y, w, h = rectangulo
-	cv.rectangle(frame, (x, y), (x+w, y+h), color, 1)
-	cv.circle(frame, (x+w//2, y+h//2), 2, Config.UI.cyan, 0)
+	cv.rectangle(image, (x, y), (x+w, y+h), color, fill)
+	cv.circle(image, (x+w//2, y+h//2), 2, Config.UI.cyan, 0)
 
 
-def dibuja_predic(frame, ROIs, predicciones):
-	"Escribe la prediccion mas popular encima  del objetivo"
+def dibuja_predic(image, ROIs, predicciones):
+	"""Dibuja encima del objetivo la prediccion con mas probabilidad
+	Parametros:
+	<ROIs> lista de rectangulos coordenadas de las zonas de interes
+	<predicciones> lista de probabilidades del objeto desde la red neuronal"""
 	if len(ROIs) != len(predicciones):
 		print(f"Error, no iguales: {len(ROIs)} {len(predicciones)}")
 		return
@@ -184,36 +210,15 @@ def dibuja_predic(frame, ROIs, predicciones):
 		print(p)
 		text = p[0][1]
 		x, y, w, h = r
-		cv.putText(frame, text,
-                    (x+5, y-5), 0, 1, Config.UI.morado, 1, 16)
-		cv.rectangle(frame, (x, y), (x+w, y+h), Config.UI.rojo_oscuro, 1)
-
-
-def dibuja_FPS(image, fps):
-	"Dibuja los FPS para comparar"
-	fps.update()
-	fps.stop()
-	# print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-	text = "FPS: {:.2f}".format(fps.fps())
-	# text = "Hola que haces"
-	cv.putText(image, text, (0, 15),
-            cv.FONT_HERSHEY_SIMPLEX, 0.5, Config.UI.rojo, 2)
-
-
-def elimina_rect_irelevantes(rectangulos, area_obj=Config.Tracker.area_min):
-	"Elimina los rectangulos no similares al buscado"
-	maximo_admitido = area_obj * (1+Config.Tracker.tolerancia)
-	minimo_admitido = area_obj * (1-Config.Tracker.tolerancia)
-	nuevos_rectangulos = []
-	for r in rectangulos:
-		area = r[2] * r[3]
-		# Agrega solo los que tienen area similar al del objetivo actual
-		if area > minimo_admitido and area < maximo_admitido:
-			nuevos_rectangulos.append(r)
-	return nuevos_rectangulos
+		cv.putText(image, text,
+					(x+5, y-5), 0, 1, Config.UI.morado, 1, 16)
+		cv.rectangle(image, (x, y), (x+w, y+h), Config.UI.rojo_oscuro, 1)
 
 
 def multi_atencion_blur(image, rectangulos):
+	""" Difumina toda la escena y  la cambia a blanco y negro excepto algunos
+	rectangulos de interes pasados en la lista y creando una mascara con ellos
+	"""
 	# Config
 	img = image.copy()
 	kernel = (47, 47)
@@ -236,7 +241,10 @@ def multi_atencion_blur(image, rectangulos):
 
 	return alphaBlend(img, img_soslayo, mascara)
 
+
 def atencion_blur(image, rect):
+	""" Difumina toda la escena y  la cambia a blanco y negro excepto un
+	rectangulo de interes, creando una mascara con el"""
 	# x1, y1, x2, y2 = rect
 	x, y, w, h = rect
 	x1, y1, x2, y2 = x, y, x+w, y+h
@@ -258,11 +266,33 @@ def atencion_blur(image, rect):
 
 
 def alphaBlend(img1, img2, mask):
-	# https://stackoverflow.com/a/48274875/3052862
-	"alphaBlend img1 and img 2 (of CV_8UC3) with mask (CV_8UC1 or CV_8UC3)"
+	"""Mezcla dos imagenes progresivamente (difuminacion alrededor de mascara)
+	Extraido de https://stackoverflow.com/a/48274875/3052862
+	alphaBlend img1 and img 2 (of CV_8UC3) with mask (CV_8UC1 or CV_8UC3)"""
 	if mask.ndim == 3 and mask.shape[-1] == 3:
 		alpha = mask/255.0
 	else:
 		alpha = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)/255.0
 	blended = cv.convertScaleAbs(img1*(1-alpha) + img2*alpha)
 	return blended
+
+
+def mascara_destruidos(image, rectangulos):
+	"""Genera una mascara negra en la imagen con los rectangulos de los ultimos
+	objetivos destruidos para no detectarlos de nuevo en la red neuronal"""
+	for r in rectangulos:
+		dibuja_rectangulo(image, r, (0, 0, 0), -1)
+
+
+def elimina_rect_irelevantes(rectangulos, area_obj=Config.Tracker.area_min):
+	"""Elimina los rectangulos no similares al buscado. Se utiliza para comparar
+	el contorno del Tracker con la deteccion de la red neuronal"""
+	maximo_admitido = area_obj * (1+Config.Tracker.tolerancia)
+	minimo_admitido = area_obj * (1-Config.Tracker.tolerancia)
+	nuevos_rectangulos = []
+	for r in rectangulos:
+		area = r[2] * r[3]
+		# Agrega solo los que tienen area similar al del objetivo actual
+		if area > minimo_admitido and area < maximo_admitido:
+			nuevos_rectangulos.append(r)
+	return nuevos_rectangulos
